846703c10ed70e18d0d1d22e97a670af488424d9
code-a1 <68858676+code-a1@users.noreply.github.com>
2025-10-17 23:15:04 +0200

    Refactor ClearButton.kt to optimize imports and enhance readability

--- composeApp/src/jvmMain/kotlin/com/codea1/coderunner/ui/ClearButton.kt
+++ composeApp/src/jvmMain/kotlin/com/codea1/coderunner/ui/ClearButton.kt
@@ -3,13 +3,9 @@ package com.codea1.coderunner.ui
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CleaningServices
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
